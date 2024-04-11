90de49fec2b5df1a42c29d296e6f01f6cb4fea86
Neal L <rlawton.cs@gmail.com>
2024-04-10 23:05:25 -0700

    Initial commit.

---
+++ Line.py
@@ -0,0 +1,59 @@
+# PROGRAMMER: Ryan Lawton
+
+# IMPORT STATEMENTS
+
+
+
+
+
+class Line:
+
+# CONSTRUCTOR
+
+    def __init__(self, point_a, point_b):
+        self.__point_a = point_a
+        self.__point_b = point_b
+
+# INSTANCE METHODS
+
+    def get_point_a(self):
+        pass
+    
+    def get_point_b(self):
+        pass
+    
+    def set_point_a(self, point_a):
+        pass
+    
+    def set_point_b(self, point_b):
+        pass
+    
+    def delta_x(self):
+        pass
+    
+    def delta_y(self):
+        pass
+    
+    def slope(self):
+        pass
+    
+    def distance(self):
+        pass
+    
+    def traversal_time(self, speed):
+        pass
+    
+    def traversal_speed(self, time):
+        pass
+    
+    def midpoint(self):
+        pass
+    
+    def x_intercept(self):
+        pass
+    
+    def y_intercept(self):
+        pass
+    
+    def export_as_comma_separated_values(self, filename):
+        pass
---
+++ LineSegments.py
@@ -0,0 +1,52 @@
+# PROGRAMMER: Ryan Lawton
+
+# IMPORT STATEMENTS
+
+
+
+
+
+class LineSegments:
+
+# CONSTRUCTOR
+    
+    def __init__(self, point_list):
+        self.__point_list = point_list
+
+# INSTANCE METHODS
+
+    def get_point(self, index):
+        pass
+    
+    def set_point(self, point, index):
+        pass
+    
+    def get_all_points(self):
+        pass
+    
+    def endpoint_distance(self):
+        pass
+    
+    def endpoint_midpoint(self):
+        pass
+    
+    def segment_distance(self, first_point_index, second_point_index):
+        pass
+    
+    def segment_midpoint(self, first_point_index, second_point_index):
+        pass
+    
+    def total_distance_traversed(self):
+        pass
+    
+    def traversal_time(self, speed):
+        pass
+    
+    def traversal_speed(self, time):
+        pass
+    
+    def traversal_efficiency_percentage(self):
+        pass
+    
+    def export_as_comma_separated_values(self, filename):
+        pass
